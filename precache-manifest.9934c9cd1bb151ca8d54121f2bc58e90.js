825a85477dc4a99f01c1317126a915cb34fc2414
Manuel Rivera <marp.developer@gmail.com>
2020-10-03 04:29:28 -0500

    Initial dist subtree commit

---
+++ precache-manifest.9934c9cd1bb151ca8d54121f2bc58e90.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053c9eef5c47bcbeb8b1",
+    "url": "/xkcd-comics/css/app.86376145.css"
+  },
+  {
+    "revision": "379d8bf8869466a46c99",
+    "url": "/xkcd-comics/css/chunk-377f964b.d48b9fde.css"
+  },
+  {
+    "revision": "21d9f4a1b2b2bffe7c6a",
+    "url": "/xkcd-comics/css/chunk-38ca0507.62936dbb.css"
+  },
+  {
+    "revision": "c14a2e6a47c1d4b08002",
+    "url": "/xkcd-comics/css/chunk-vendors.bfffc268.css"
+  },
+  {
+    "revision": "92f9daeb50bd7d61210a92bb90124066",
+    "url": "/xkcd-comics/index.html"
+  },
+  {
+    "revision": "053c9eef5c47bcbeb8b1",
+    "url": "/xkcd-comics/js/app.3013a914.js"
+  },
+  {
+    "revision": "379d8bf8869466a46c99",
+    "url": "/xkcd-comics/js/chunk-377f964b.b88f45bf.js"
+  },
+  {
+    "revision": "21d9f4a1b2b2bffe7c6a",
+    "url": "/xkcd-comics/js/chunk-38ca0507.aef62e2b.js"
+  },
+  {
+    "revision": "c14a2e6a47c1d4b08002",
+    "url": "/xkcd-comics/js/chunk-vendors.f9585663.js"
+  },
+  {
+    "revision": "f36cfcbcdb1fd26d90b6d4c699c1cdf5",
+    "url": "/xkcd-comics/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xkcd-comics/robots.txt"
+  }
+]);
